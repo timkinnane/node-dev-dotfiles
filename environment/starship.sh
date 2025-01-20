496ca4cb6a8faaf4e06a647250762ae84b5aa7f2
Tim Kinnane <tim-kinnane@pluralsight.com>
2025-01-21 10:17:00 +1100

    fix: starship prompt theme load order

--- environment/starship.sh
+++ environment/starship.sh
@@ -1,6 +1,8 @@
+# Use Dracula Theme for shell and prompt
+LSCOLORS=ExFxBxDxCxegedabagacad
+export BAT_THEME="Dracula"
+
 # Starship
 # https://starship.rs/
 # Fix unsightly colours in prompt ;)
 eval "$(starship init zsh)"
-LSCOLORS=ExFxBxDxCxegedabagacad
-export BAT_THEME="Dracula"
